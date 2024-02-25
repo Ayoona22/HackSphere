59b1699d1c23b8b10db6d382a8ea844f2790ae3c
Sample <22ct014@mgits.ac.in>
2024-02-25 11:36:49 +0530

    Connected all the webpages

--- Medsql/app.py
+++ Medsql/app.py
@@ -18,6 +18,25 @@ def Signup():
 def about():
     return render_template('about.html')
 
+@app.route('/doctor_portal')
+def doctor_portal():
+    return render_template('doctor_portal.html')
+
+@app.route('/patient_portal')
+def patient_portal():
+    return render_template('patient_portal.html')
+
+@app.route('/alert')
+def alert():
+    return render_template('alert.html')
+
+@app.route('/TestResults')
+def TestResults():
+    return render_template('TestResults.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
