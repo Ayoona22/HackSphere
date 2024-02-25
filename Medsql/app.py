764174613b0c4b51422924bb0ca088161bd1d988
Sample <22ct014@mgits.ac.in>
2024-02-25 08:58:00 +0530

    Created portals for patients and doctors

--- Medsql/app.py
+++ Medsql/app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('frontpage.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/Signup')
+def Signup():
+    return render_template('Signup.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
